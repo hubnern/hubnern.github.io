17dc618f1ae4280b3dd4ac65b34c6c149d8776c2
Nicolas hubner <nicolas.hubner@u-bordeaux.fr>
2024-04-19 15:35:38 +0200

    clear

--- src/routes/+page.svelte
+++
@@ -1,8 +0,0 @@
-<h1>La thèse de Nicolas Hubner</h1>
-<p>Pour annoter les lignes des diff, <a href="annotation">cliquez ici</a></p>
-
-<style>
-    h1, p, a {
-        font-family: sans-serif;
-    }
-</style>
